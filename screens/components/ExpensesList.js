5f55dc2f8cc9a2c2edaa584cf017bbd83ab814cf
Ferer Atlus <benedictcontawe@gmail.com>
2025-03-04 14:00:12 +0800

    add ExpenseItem cell added four items in DUMMY_EXPENSES

--- screens/components/ExpensesList.js
+++ screens/components/ExpensesList.js
@@ -1,15 +1,18 @@
-import { FlatList, Text } from 'react-native';
+import { FlatList } from 'react-native';
+import ExpenseItem from './ExpenseItem';
 
 function renderExpenseItem(itemData) {
-    return <Text>{itemData.item.description}</Text>;
-  }
+    return <ExpenseItem {...itemData.item} />;
+}
 
-export const ExpensesList = ({ expenses }) => {
-    return <FlatList 
-        data={expenses} 
+function ExpensesList({ expenses }) {
+    return (
+        <FlatList
+        data={expenses}
         renderItem={renderExpenseItem}
-        keyExtractor={(item) => item.id }
-    />;
+        keyExtractor={(item) => item.id}
+        />
+    );
 }
 
 export default ExpensesList;
