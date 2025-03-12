521055d792892c07bd43672b5046d62ac0022f5f
Ferer Atlus <benedictcontawe@gmail.com>
2025-03-12 18:17:06 +0800

    add code for switchAuthModeHandler navigation stack replace screen

--- components/AuthContent.js
+++ components/AuthContent.js
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import { Alert, StyleSheet, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import FlatButtonComponent from './FlatButtonComponent';
 import AuthForm from './AuthForm';
 import { Colors } from '../constants/styles';
 
 function AuthContent({ isLogin, onAuthenticate }) {
+  const navigation = useNavigation();
 
   const [credentialsInvalid, setCredentialsInvalid] = useState({
     email: false,
@@ -14,7 +16,11 @@ function AuthContent({ isLogin, onAuthenticate }) {
   });
 
   function switchAuthModeHandler() {
-    // Todo
+    if (isLogin) {
+      navigation.replace('Signup');
+    } else {
+      navigation.replace('Login');
+    }
   }
 
   function submitHandler(credentials) {
