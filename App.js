278197fe07d9b6916b6c61f925141a80bec923e9
Ferer Atlus <benedictcontawe@gmail.com>
2025-03-13 16:11:14 +0800

    Added Icon Button on headerRight Added Style Colors

--- App.js
+++ App.js
@@ -4,19 +4,47 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import AllPlaces from './screens/AllPlaces';
 import AddPlace from './screens/AddPlace';
+import IconButton from './components/IconButton';
+import { Colors } from './constants/colors';
+import { Fragment } from 'react';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <>
+    <Fragment>
       <StatusBar style='dark' />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="AllPlaces" component={AllPlaces} />
-          <Stack.Screen name="AddPlace" component={AddPlace} />
+      <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.primary500 },
+            headerTintColor: Colors.gray700,
+            contentStyle: { backgroundColor: Colors.gray700 },
+          }}
+        >
+        <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add"
+                  size={24}
+                  color={tintColor}
+                  onPress={() => navigation.navigate('AddPlace')}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen
+            name="AddPlace"
+            component={AddPlace}
+            options={{
+              title: 'Add a new Place',
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
-    </>
+    </Fragment>
   );
 }
