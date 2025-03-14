bb4010ab71fe2b4b77a83abdc472e5d1786a90ae
Ferer Atlus <benedictcontawe@gmail.com>
2025-03-14 21:27:26 +0800

    Update Place Screen

--- components/PlacesList.js
+++ components/PlacesList.js
@@ -1,5 +1,5 @@
 import { FlatList, StyleSheet, Text, View } from 'react-native';
-
+import { Colors } from '../constants/colors';
 import PlaceItem from './PlaceItem';
 
 function PlacesList({ places }) {
@@ -15,6 +15,7 @@ function PlacesList({ places }) {
 
   return (
     <FlatList
+      style={styles.list}
       data={places}
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => <PlaceItem place={item} />}
@@ -25,6 +26,9 @@ function PlacesList({ places }) {
 export default PlacesList;
 
 const styles = StyleSheet.create({
+  list: {
+    margin: 24,
+  },
   fallbackContainer: {
     flex: 1,
     justifyContent: 'center',
@@ -32,5 +36,6 @@ const styles = StyleSheet.create({
   },
   fallbackText: {
     fontSize: 16,
+    color: Colors.primary200,
   },
 });
