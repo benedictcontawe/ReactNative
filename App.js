f460acdc637f2b242fd7111ef140fde79ebb0380
Ferer Atlus <benedictcontawe@gmail.com>
2025-03-14 19:27:22 +0800

    added react native maps

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import AllPlaces from './screens/AllPlaces';
 import AddPlace from './screens/AddPlace';
+import Map from './screens/Map';
 import IconButton from './components/IconButton';
 import { Colors } from './constants/colors';
 import { Fragment } from 'react';
@@ -43,6 +44,7 @@ export default function App() {
               title: 'Add a new Place',
             }}
           />
+          <Stack.Screen name="Map" component={Map} />
         </Stack.Navigator>
       </NavigationContainer>
     </Fragment>
--- components/LocationPicker.js
+++ components/LocationPicker.js
@@ -5,11 +5,13 @@ import {
   useForegroundPermissions,
   PermissionStatus,
 } from 'expo-location';
+import { useNavigation } from '@react-navigation/native';
 import { Colors } from '../constants/colors';
 import OutlinedButton from './OutlinedButton';
 import { getMapPreview } from '../util/location';
 
 function LocationPicker() {
+  const navigation = useNavigation();
   const [pickedLocation, setPickedLocation] = useState();
   const [locationPermissionInformation, requestPermission] = useForegroundPermissions();
 
@@ -44,7 +46,9 @@ function LocationPicker() {
     });
   }
 
-  function pickOnMapHandler() {}
+  function pickOnMapHandler() {
+    navigation.navigate('Map');
+  }
 
   let locationPreview = <Text>No location picked yet.</Text>;
 
