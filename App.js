c0b88dd4cb69e2a7a4a217fa2d582e424bdfbefc
Ferer Atlus <benedictcontawe@gmail.com>
2025-03-19 11:36:55 +0800

    Schedule Notification Notification Listener Response Notification Listener

--- App.js
+++ App.js
@@ -1,10 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications'
+import { useEffect } from 'react';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true,
+    };
+  }
+});
 
 export default function App() {
+  useEffect(() => {
+    const subscription1 = Notifications.addNotificationReceivedListener((notification) => {
+      console.log('Notification Received');
+      console.log(notification)
+      const userName = notification.request.content.data.userName;
+      console.log(userName)
+    });
+    const subscription2 = Notifications.addNotificationResponseReceivedListener((response) => {
+      console.log('Notification Response Received');
+      console.log(response)
+      const userName = notification.request.content.data.userName;
+      console.log(userName)
+    });
+    return () => {
+      subscription1.remove();
+      subscription2.remove();
+    };
+  }, []);
+
+  function scheduleNotificationHandler() {
+    Notifications.scheduleNotificationAsync({
+      content: { 
+        title: 'My first local notification', 
+        body: 'This is the body of the notification.',
+        data: { userName: 'Max' },
+      },
+      trigger: {
+        seconds: 5
+      },
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title='Schedule Notification' onPress={scheduleNotificationHandler}></Button>
       <StatusBar style="auto" />
     </View>
   );
