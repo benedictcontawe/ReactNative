a8c7beb3dc2383481d9b3f165ed15d91df6dd1d3
Ben <benedictcontawe@gmail.com>
2025-10-27 17:45:07 +0800

    new branch for sample of Bottom Tab Navigator

---
+++ App.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/HomeScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+  const BottomTab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <BottomTab.Navigator
+      initialRouteName="Home"
+     >
+        <BottomTab.Screen name="Home" component={HomeScreen} />
+        <BottomTab.Screen name="Settings" component={SettingsScreen} />
+      </BottomTab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
