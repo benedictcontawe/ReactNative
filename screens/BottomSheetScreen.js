3b956b35f65132bceccebc9e1ee690a3ed914223
Benedict Contawe <benedictcontawe@gmail.com>
2026-02-11 17:09:18 +0800

    added BottomSheetTextInput

--- screens/BottomSheetScreen.js
+++ screens/BottomSheetScreen.js
@@ -1,8 +1,9 @@
-import React, { useMemo, useRef } from 'react';
+import React, { useState, useMemo, useRef } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
-import { BottomSheetBackdrop, BottomSheetModal, BottomSheetView } from '@gorhom/bottom-sheet';
+import { BottomSheetBackdrop, BottomSheetModal, BottomSheetView, BottomSheetTextInput } from '@gorhom/bottom-sheet';
 
 const BottomSheetScreen = () => {
+  const [bottomSheetTextInputValue, setBottomSheetTextInputValue] = useState('');
   const bottomSheetModalRef = useRef(null);
   const snapPoints = useMemo(() => [`25%`, `50%`, `70%`], []);//const snapPoints = useMemo(() => [`45%`], []);
   const handleShowSheet = () => bottomSheetModalRef.current?.present();
@@ -30,16 +31,30 @@ const BottomSheetScreen = () => {
             backdropComponent={renderBackdrop}
             enablePanDownToClose={true}
             enableContentPanningGesture={true}
-            enableHandlePanningGesture={true} >
-            <BottomSheetView style={styles.contentContainer}>
-                <Text>This is Bottomsheet</Text>
-                <Text style={{ fontSize: 16 }}>Sheet is Open!</Text>
-                <Button 
+            enableHandlePanningGesture={true}        
+            android_keyboardInputMode="adjustResize"
+            keyboardBehavior="interactive"
+            keyboardBlurBehavior="restore"
+        >
+        <BottomSheetView style={styles.contentContainer}>
+            <Text>This is Bottomsheet</Text>
+            <Text style={{ fontSize: 16 }}>Sheet is Open!</Text>
+            <Button 
                     title="Tap to Close" 
                     onPress={handleHideSheet}
                 />
-            </BottomSheetView>
-        </BottomSheetModal>
+            <BottomSheetTextInput
+              style={styles.input} // Needs styling to be visible
+              placeholder="Bottom Sheet Text Input"
+              placeholderTextColor="#666"
+              keyboardType="default"
+              value={bottomSheetTextInputValue}
+              onChangeText={setBottomSheetTextInputValue}
+            />
+
+            <Button title="Submit" onPress={() => console.log(bottomSheetTextInputValue)} />
+        </BottomSheetView>
+      </BottomSheetModal>
     </View>
   );
 };
@@ -52,11 +67,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#f0a057',
   },
-  headerText: {
-    fontSize: 20,
+  input: {
+    marginTop: 8,
+    marginBottom: 10,
+    borderRadius: 10,
+    fontSize: 16,
+    lineHeight: 20,
+    padding: 12,
+    backgroundColor: 'rgba(255, 255, 255, 0.5)', // Light background to see it on gray
+    width: '100%',
+    color: 'black',
+  },
+  title: {
+    fontSize: 18,
     fontWeight: 'bold',
-    marginBottom: 20,
-    color: '#0055a4',
+    marginBottom: 10,
   },
   contentContainer: {
     flex: 1,
