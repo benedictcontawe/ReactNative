70907092585fb3265ff1b4b9d0138c4615a45459
Benedict Contawe <benedictcontawe@gmail.com>
2026-02-12 12:06:27 +0800

    add handleInputFocus for keyboard of bottom sheet screen

--- screens/BottomSheetScreen.js
+++ screens/BottomSheetScreen.js
@@ -1,58 +1,63 @@
-import React, { useState, useMemo, useRef } from 'react';
+import { useState, useMemo, useRef, useCallback } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { BottomSheetBackdrop, BottomSheetModal, BottomSheetView, BottomSheetTextInput } from '@gorhom/bottom-sheet';
 
 const BottomSheetScreen = () => {
   const [bottomSheetTextInputValue, setBottomSheetTextInputValue] = useState('');
   const bottomSheetModalRef = useRef(null);
-  const snapPoints = useMemo(() => [`25%`, `50%`, `70%`], []);//const snapPoints = useMemo(() => [`45%`], []);
+  const snapPoints = useMemo(() => [`30%`, `50%`, `75%`], []);
   const handleShowSheet = () => bottomSheetModalRef.current?.present();
   const handleHideSheet = () => bottomSheetModalRef.current?.close();
+
+  const handleInputFocus = useCallback(() => {
+    bottomSheetModalRef.current?.snapToIndex(3);
+  }, []);
+
   const renderBackdrop = (props) => (
-  <BottomSheetBackdrop 
-    {...props} 
-    appearsOnIndex={0}
-    disappearsOnIndex={-1}
-    pressBehavior='close'
-    style={{ backgroundColor: 'rgba(0, 0, 0, 0.7)' }}
-  />
-);
+    <BottomSheetBackdrop 
+      {...props} 
+      appearsOnIndex={0}
+      disappearsOnIndex={-1}
+      pressBehavior='close'
+      style={{ backgroundColor: 'rgba(0, 0, 0, 0.7)' }}
+    />
+  );
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>BottomSheet Screen</Text>
       <Button title="Show Bottom Sheet" onPress={handleShowSheet} />
       <Button title="Hide Bottom Sheet" onPress={handleHideSheet} />
-        <BottomSheetModal 
-            ref={bottomSheetModalRef} 
-            index={0}
-            snapPoints={snapPoints}
-            handleIndicatorStyle={{ backgroundColor: 'black' }}
-            backgroundStyle={{ backgroundColor: '#CCC' }}
-            backdropComponent={renderBackdrop}
-            enablePanDownToClose={true}
-            enableContentPanningGesture={true}
-            enableHandlePanningGesture={true}        
-            android_keyboardInputMode="adjustResize"
-            keyboardBehavior="interactive"
-            keyboardBlurBehavior="restore"
-        >
+      <BottomSheetModal 
+        ref={bottomSheetModalRef} 
+        index={0}
+        snapPoints={snapPoints}
+        handleIndicatorStyle={{ backgroundColor: 'black' }}
+        backgroundStyle={{ backgroundColor: '#CCC' }}
+        backdropComponent={renderBackdrop}
+        enablePanDownToClose={true}
+        enableContentPanningGesture={true}
+        enableHandlePanningGesture={true}      
+        android_keyboardInputMode="adjustResize"
+        keyboardBehavior="interactive"
+        keyboardBlurBehavior="restore"
+      >
         <BottomSheetView style={styles.contentContainer}>
-            <Text>This is Bottomsheet</Text>
-            <Text style={{ fontSize: 16 }}>Sheet is Open!</Text>
-            <Button 
-                    title="Tap to Close" 
-                    onPress={handleHideSheet}
-                />
-            <BottomSheetTextInput
-              style={styles.input} // Needs styling to be visible
-              placeholder="Bottom Sheet Text Input"
-              placeholderTextColor="#666"
-              keyboardType="default"
-              value={bottomSheetTextInputValue}
-              onChangeText={setBottomSheetTextInputValue}
-            />
-
-            <Button title="Submit" onPress={() => console.log(bottomSheetTextInputValue)} />
+          <Text>This is Bottomsheet</Text>
+          <Text style={{ fontSize: 16 }}>Sheet is Open!</Text>
+          <Button 
+            title="Tap to Close" 
+            onPress={handleHideSheet}
+          />
+          <BottomSheetTextInput
+            style={styles.input}
+            placeholder="Bottom Sheet Text Input"
+            placeholderTextColor="#666"
+            keyboardType="default"
+            value={bottomSheetTextInputValue}
+            onChangeText={setBottomSheetTextInputValue}
+            onFocus={handleInputFocus} 
+          />
+          <Button title="Submit" onPress={() => console.log(bottomSheetTextInputValue)} />
         </BottomSheetView>
       </BottomSheetModal>
     </View>
